62f63bd88dc2a72152cf2d34c2a8eb6891c10b08
JeanGoumez2001 <adrielgo.mez123@gmail.com>
2023-11-29 23:24:49 -0500

    try varied dependencies for create-view pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+ 
+import { Component, Input, Output, EventEmitter,ElementRef, ViewChild } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
+import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdfmake';
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+  src = '';
+  isPdfLoaded = false;
+  private pdf: PDFDocumentProxy | any;
+
+
+  @ViewChild('pdfContent', { static: false }) pdfContent: any;
+  displayAlert: boolean = false;
+  
+    openAlert(): void {
+        this.displayAlert = true;
+    }
+
+    closeAlert(): void {
+        this.displayAlert = false;
+    }
+    
+    onLoaded(pdf: PDFDocumentProxy) {
+      this.pdf = pdf;
+      this.isPdfLoaded = true;
+    }
+    ngOnInit() {
+      this.src = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+    }
+
+    print() {
+      this.pdf.getData().then((u8) => {
+        let blob = new Blob([u8.buffer], {
+            type: 'application/pdf'
+        });
+
+        const blobUrl = window.URL.createObjectURL((blob));
+        const iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        iframe.src = blobUrl;
+        document.body.appendChild(iframe);
+        iframe.contentWindow?.print();
+    });
+    }
 }
